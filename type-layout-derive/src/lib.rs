aacc3f053ee6cce278a723dcade584e55d8f91c7
Lucien Greathouse <me@lpghatguy.com>
2020-09-19 18:08:12 -0700

    Revert "Add attribute for renaming crate"
    This reverts commit 065f66532817fe8d314c325e60bae9724929e6f3.

--- type-layout-derive/src/lib.rs
+++ type-layout-derive/src/lib.rs
@@ -1,36 +1,33 @@
 extern crate proc_macro;
 
-use proc_macro::TokenStream as CompilerTokenStream;
+use proc_macro::TokenStream;
 
-use proc_macro2::{Ident, Literal, TokenStream};
+use proc_macro2::{Ident, Literal};
 use quote::{quote, quote_spanned, ToTokens};
-use syn::{parse_macro_input, spanned::Spanned, Data, DeriveInput, Fields, Meta, NestedMeta};
+use syn::{parse_macro_input, spanned::Spanned, Data, DeriveInput, Fields};
 
 #[proc_macro_derive(TypeLayout)]
-pub fn derive_type_layout(input: CompilerTokenStream) -> CompilerTokenStream {
+pub fn derive_type_layout(input: TokenStream) -> TokenStream {
     // Parse the input tokens into a syntax tree
     let input = parse_macro_input!(input as DeriveInput);
-
-    let context = Context::get(&input);
-    let crate_path = &context.crate_path;
 
     // Used in the quasi-quotation below as `#name`.
     let name = input.ident;
     let name_str = Literal::string(&name.to_string());
 
     let (impl_generics, ty_generics, where_clause) = input.generics.split_for_impl();
-    let layout = layout_of_type(&context, &name, &input.data);
+    let layout = layout_of_type(&name, &input.data);
 
     // Build the output, possibly using quasi-quotation
     let expanded = quote! {
-        impl #impl_generics #crate_path::TypeLayout for #name #ty_generics #where_clause {
-            fn type_layout() -> #crate_path::TypeLayoutInfo {
+        impl #impl_generics ::type_layout::TypeLayout for #name #ty_generics #where_clause {
+            fn type_layout() -> ::type_layout::TypeLayoutInfo {
                 let mut last_field_end = 0;
                 let mut fields = Vec::new();
 
                 #layout
 
-                #crate_path::TypeLayoutInfo {
+                ::type_layout::TypeLayoutInfo {
                     name: ::std::borrow::Cow::Borrowed(#name_str),
                     size: std::mem::size_of::<#name>(),
                     alignment: ::std::mem::align_of::<#name>(),
@@ -41,52 +38,10 @@ pub fn derive_type_layout(input: CompilerTokenStream) -> CompilerTokenStream {
     };
 
     // Hand the output tokens back to the compiler
-    CompilerTokenStream::from(expanded)
+    TokenStream::from(expanded)
 }
 
-struct Context {
-    crate_path: TokenStream,
-}
-
-impl Context {
-    fn get(input: &DeriveInput) -> Self {
-        let mut crate_path = None;
-
-        for attr in &input.attrs {
-            match attr.path.get_ident() {
-                Some(name) if name == "type_layout" => {}
-                _ => continue,
-            }
-
-            let meta = match attr.parse_meta() {
-                Ok(Meta::List(meta)) => meta.nested.into_iter().next().unwrap(),
-                Ok(_) => {
-                    panic!("malformed attribute");
-                }
-                Err(err) => {
-                    panic!("malformed attribute: {}", err);
-                }
-            };
-
-            match meta {
-                NestedMeta::Meta(Meta::Path(path)) => {
-                    crate_path = Some(path.clone());
-                }
-                _ => panic!("Unknown type_layout metadata"),
-            }
-        }
-
-        let crate_path = crate_path
-            .map(|path| path.to_token_stream())
-            .unwrap_or_else(|| quote!(::type_layout));
-
-        Self { crate_path }
-    }
-}
-
-fn layout_of_type(context: &Context, struct_name: &Ident, data: &Data) -> proc_macro2::TokenStream {
-    let crate_path = &context.crate_path;
-
+fn layout_of_type(struct_name: &Ident, data: &Data) -> proc_macro2::TokenStream {
     match data {
         Data::Struct(data) => match &data.fields {
             Fields::Named(fields) => {
@@ -100,15 +55,15 @@ fn layout_of_type(context: &Context, struct_name: &Ident, data: &Data) -> proc_m
                         #[allow(unused_assignments)]
                         {
                             let size = ::std::mem::size_of::<#field_ty>();
-                            let offset = #crate_path::memoffset::offset_of!(#struct_name, #field_name);
+                            let offset = ::type_layout::memoffset::offset_of!(#struct_name, #field_name);
 
                             if offset > last_field_end {
-                                fields.push(#crate_path::Field::Padding {
+                                fields.push(::type_layout::Field::Padding {
                                     size: offset - last_field_end
                                 });
                             }
 
-                            fields.push(#crate_path::Field::Field {
+                            fields.push(::type_layout::Field::Field {
                                 name: ::std::borrow::Cow::Borrowed(#field_name_str),
                                 ty: ::std::borrow::Cow::Borrowed(#field_ty_str),
                                 size,
@@ -124,7 +79,7 @@ fn layout_of_type(context: &Context, struct_name: &Ident, data: &Data) -> proc_m
 
                     let struct_size = ::std::mem::size_of::<#struct_name>();
                     if struct_size > last_field_end {
-                        fields.push(#crate_path::Field::Padding {
+                        fields.push(::type_layout::Field::Padding {
                             size: struct_size - last_field_end,
                         });
                     }
